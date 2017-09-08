b7e5479307ee62435257c9b74fe0710adf3c81cd
Fenmar <xfenmar@gmail.com>
2017-09-08 21:00:14 +0200

    have all menu entries visible at all times

--- src/contentSelection/YearEntry.js
+++ src/contentSelection/YearEntry.js
@@ -22,52 +22,37 @@ class YearEntry extends Component {
     newTab.focus();
   }
 
-  mouseEnter = evt => {
-    this.setState({
-      hovered: true
-    });
-  }
-
-  mouseLeave = evt => {
-    this.setState({
-      hovered: false
-    });
-  }
-
   render() {
     const isActive = this.props.match.params.year === this.props.title;
     const entryClass = this.props.theme ? "archive-entry year-entry" : "archive-entry year-entry-no-theme";
     return (
-      <Menu.Item onMouseEnter={this.mouseEnter} onMouseLeave={this.mouseLeave} active={isActive} className={entryClass}>
+      <Menu.Item active={isActive} className={entryClass}>
         <Header as="h4">{this.props.title}</Header>
         <p>{this.props.theme}</p>
-        {
-          (isActive || this.state.hovered) &&
-          <Menu attached compact className="archive-inline-menu">
-            <Dropdown disabled={!this.props.daily} item text="Daily" className={isActive && this.props.match.params.type === 'daily' ? 'active' : ''}>
-              <Dropdown.Menu>
-                {this.props.daily && this.props.daily.map((issue, idx) => 
-                  <Dropdown.Item 
-                    key={idx} 
-                    as='a'
-                    active={issue.title === this.props.match.params.issue} 
-                    onClick={this.select('daily', issue.title)}
-                    href={issue.url}
-                  >
-                    {issue.title}
-                  </Dropdown.Item>
-                )}
-              </Dropdown.Menu>
-            </Dropdown>
-            {
-              this.props.conbook ?
-                <Menu.Item active={isActive && this.props.match.params.type === 'conbook'} onClick={this.select('conbook')} href={this.props.conbook}>Conbook</Menu.Item>
-              :
-              <Menu.Item className="archive-disabled-conbook">Conbook</Menu.Item>
-            }
-            <Menu.Item onClick={this.openWebsite} href={this.props.website}>Website</Menu.Item>
-          </Menu>
-        }
+        <Menu attached compact className="archive-inline-menu">
+          <Dropdown disabled={!this.props.daily} item text="Daily" className={isActive && this.props.match.params.type === 'daily' ? 'active' : ''}>
+            <Dropdown.Menu>
+              {this.props.daily && this.props.daily.map((issue, idx) => 
+                <Dropdown.Item 
+                  key={idx} 
+                  as='a'
+                  active={issue.title === this.props.match.params.issue} 
+                  onClick={this.select('daily', issue.title)}
+                  href={issue.url}
+                >
+                  {issue.title}
+                </Dropdown.Item>
+              )}
+            </Dropdown.Menu>
+          </Dropdown>
+          {
+            this.props.conbook ?
+              <Menu.Item active={isActive && this.props.match.params.type === 'conbook'} onClick={this.select('conbook')} href={this.props.conbook}>Conbook</Menu.Item>
+            :
+            <Menu.Item className="archive-disabled-conbook">Conbook</Menu.Item>
+          }
+          <Menu.Item onClick={this.openWebsite} href={this.props.website}>Website</Menu.Item>
+        </Menu>
       </Menu.Item>
     );
   }
