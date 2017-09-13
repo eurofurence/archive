0d79d15d8c00fc22bf113a125800df9bfb0ab8b5
Fenmar <xfenmar@gmail.com>
2017-09-13 15:50:37 +0200

    add fonts locally

--- scripts/purifyCss.js
+++ scripts/purifyCss.js
@@ -2,15 +2,19 @@ const purifycss = require("purify-css");
 const fs = require('fs');
 const path = require('path');
 
-const fileName = fs.readdirSync('./build/static/css').find(el => path.extname(el) === '.css');
+const fileName = './build/static/css/' + fs.readdirSync('./build/static/css').find(el => path.extname(el) === '.css');
 
 let content = ['./build/index.html', './build/static/*.html'];
 let css = [fileName];
 let options = {
-    output: fileName,
     minify: true
 }
 
 console.log('purifying CSS of ' + fileName);
 
-purifycss(content, css, options);
+purifycss(content, css, options, function(purified) {
+  const out = purified
+    .replace("https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic&subset=latin", "../../fonts/lato.css");
+
+    fs.writeFileSync(fileName, out);
+});
